e6d6f3e6c8b65b3ef47d930fb4bc591afa1c616e
learnwithjacksun <codewithjacksun@gmail.com>
2025-06-29 21:57:11 +0100

    refactor: Remove Header and Hero components along with their exports

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,10 +1,10 @@
-import { Header } from "@/components/main";
+// import { Header } from "@/components/main";
 import { Footer } from "@/components/ui";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main className="space-y-10 md:space-y-20 py-10">{children}</main>
       <Footer />
     </>
